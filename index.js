fbf9bff05c7f086beb83fa15916f69f8488b53bf
asad32432t234231 <124665090+asad32432t234231@users.noreply.github.com>
2023-02-10 20:20:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const y2 = require("y2mate-api");
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Api Working')
+})
+
+app.get('/api', (req, res) => {
+    var url = req.query.url;
+    const data = y2.GetVideo(url).then(response => {
+        res.send(response)
+    });
+
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
